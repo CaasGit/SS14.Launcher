12a5d7b4d440e521193197912661b72fdf34013b
Cassandra <87243814+CaasGit@users.noreply.github.com>
2025-12-13 13:03:51 -0800

    Set UseSystemSqlite.

--- fix_ss14_libs.sh
+++ fix_ss14_libs.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 cd SS14.Loader/bin/Debug/net10.0/runtimes/linux-x64/native
-rm libe_sqlite3.so libsodium.so
+rm libsodium.so
 ln -s /usr/lib64/libsodium.so.26.2.0 libsodium.so
-ln -s /usr/lib64/libsqlite3.so libe_sqlite3.so
