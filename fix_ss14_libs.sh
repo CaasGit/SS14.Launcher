997d62c73efd4a5400a6189ee92fda261e3b4ef1
Cassandra <87243814+CaasGit@users.noreply.github.com>
2026-02-12 20:27:47 -0800

    Update for latest Fedora.

--- fix_ss14_libs.sh
+++ fix_ss14_libs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd SS14.Loader/bin/Debug/net10.0/runtimes/linux-x64/native
 rm libsodium.so
-ln -s /usr/lib64/libsodium.so.26.2.0 libsodium.so
+ln -s /usr/lib64/libsodium.so.26 libsodium.so
