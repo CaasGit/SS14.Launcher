c4f11706dae77f742cd966d5d8347f495741fd2b
Cassandra <87243814+CaasGit@users.noreply.github.com>
2026-02-22 11:39:36 -0800

    Remove libsodium from launcher too.

--- fix_ss14_libs.sh
+++ fix_ss14_libs.sh
@@ -2,3 +2,6 @@
 cd SS14.Loader/bin/Debug/net10.0/runtimes/linux-x64/native
 rm libsodium.so
 ln -s /usr/lib64/libsodium.so.26 libsodium.so
+cd SS14.Loader/bin/Release/net10.0/runtimes/linux-x64/native
+rm libsodium.so
+ln -s /usr/lib64/libsodium.so.26 libsodium.so
