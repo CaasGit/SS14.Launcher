5bc06643dd5493c138e6c8da39ccb491b2b34951
Cassandra <87243814+CaasGit@users.noreply.github.com>
2026-04-01 23:20:09 -0700

    Update script.

--- fix_ss14_libs.sh
+++ fix_ss14_libs.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 for app in Loader Launcher
+do
     for build in Debug Release
     do
-        rm SS14.${app}/bin/${build}/net10.0/runtimes/linux-x64/native/libsodium.so && ln -s /usr/lib64/libsodium.so.26 SS14.${app}/bin/${build}/net10.0/runtimes/linux-x64/native/libsodium.so
+    if [ -f SS14.${app}/bin/${build}/net10.0/runtimes/linux-x64/native/libsodium.so ]; then
+            rm SS14.${app}/bin/${build}/net10.0/runtimes/linux-x64/native/libsodium.so && ln -s /usr/lib64/libsodium.so.26 SS14.${app}/bin/${build}/net10.0/runtimes/linux-x64/native/libsodium.so
+    fi
     done
 done
